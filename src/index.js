c4984342a1db2258c8b1230e0e9b5a2eace19244
navdeep <pandatnavvu@gmail.com>
2022-08-16 21:51:07 +0530

    mongostrt

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
+mongoose.connect("mongodb+srv://navdeep:5iDjE9sQTxuy0T6v@cluster0.oxrsqmy.mongodb.net/Navdeep52-DB"
 , {
    useNewUrlParser: true 
 }
