b90af0e5e72656b3ac315e83898af372fa8fd5d8
navdeep <pandatnavvu@gmail.com>
2022-08-18 22:50:52 +0530

    votingstatus

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,99 @@
 const express = require('express');
+//const bodyParser = require('body-parser');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
+// let players =
+//    [
+//        {
+//            "name": "manish",
+//            "dob": "1/1/1995",
+//            "gender": "male",
+//            "city": "jalandhar",
+//            "sports": [
+//                "swimming"
+//            ]
+//        },
+//        {
+//            "name": "gopal",
+//            "dob": "1/09/1995",
+//            "gender": "male",
+//            "city": "delhi",
+//            "sports": [
+//                "soccer"
+//            ],
+//        },
+//        {
+//            "name": "lokesh",
+//            "dob": "1/1/1990",
+//            "gender": "male",
+//            "city": "mumbai",
+//            "sports": [
+//                "soccer"
+//            ],
+//        },
+//    ]
+//    router.post('/playersall', function (req, res) {
+//     let lastplayer = req.body;
+//        //LOGIC WILL COME HERE
+//        for( let newplayer in players){
+//         if(players[newplayer].name === lastplayer.name){
+//             req.send("this player is already exist")
+//         }
+        
+//        }
+//        players.push(lastplayer)
+//        res.json( players);
+//    })
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
+let persons =[
+    {
+        name: "pk",
+        age :23,
+        votingstatus:false
 
+    },
+    {
+        name: "sk",
+        age :16,
+        votingstatus:false
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
+    },
+    {
+        name: "ab",
+        age :10,
+        votingstatus:false
 
+    },
+    {
+        name: "bk",
+        age :70,
+        votingstatus:false
 
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
+    },
+    {
+        name: "pk",
+        age :45,
+        votingstatus:false
 
+    }
+    
+]
+router.post('/votingage', function (req, res) {
+    let blankarray = [];
+    
+        let voteage = req.query.age;
+           //LOGIC WILL COME HERE
+           for( let person1 in persons){
+             if(persons[person1].age>voteage){
+                persons[person1].votingstatus = true;
+                blankarray.push(persons[person1])
 
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
-
-module.exports = router;
+             }
+            
+           }
+    
+    //        players.push(lastplayer)
+           res.json(blankarray);
+       })
+    
+ module.exports = router;
