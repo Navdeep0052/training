5b5dc56243db5c29e8c3c4e23010c4a5f8e96987
navdeep <pandatnavvu@gmail.com>
2022-08-19 12:17:50 +0530

    voting/player

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,51 +1,52 @@
 const express = require('express');
 //const bodyParser = require('body-parser');
 const router = express.Router();
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
 
-// let players =
-//    [
-//        {
-//            "name": "manish",
-//            "dob": "1/1/1995",
-//            "gender": "male",
-//            "city": "jalandhar",
-//            "sports": [
-//                "swimming"
-//            ]
-//        },
-//        {
-//            "name": "gopal",
-//            "dob": "1/09/1995",
-//            "gender": "male",
-//            "city": "delhi",
-//            "sports": [
-//                "soccer"
-//            ],
-//        },
-//        {
-//            "name": "lokesh",
-//            "dob": "1/1/1990",
-//            "gender": "male",
-//            "city": "mumbai",
-//            "sports": [
-//                "soccer"
-//            ],
-//        },
-//    ]
-//    router.post('/playersall', function (req, res) {
-//     let lastplayer = req.body;
-//        //LOGIC WILL COME HERE
-//        for( let newplayer in players){
-//         if(players[newplayer].name === lastplayer.name){
-//             req.send("this player is already exist")
-//         }
-        
-//        }
-//        players.push(lastplayer)
-//        res.json( players);
-//    })
+   router.post('/playersall', function (req, res) {
+    let lastplayer = req.body;
+       //LOGIC WILL COME HERE
+       for(let allplay of players){
+        if(allplay.name ===lastplayer.name){
+             res.json("this player is already exist")
+        }
+       }
+       players.push(lastplayer)
+       res.json( players);
+   })
+  
+module.exports = router;
 
-let persons =[
+/* let persons =[
     {
         name: "pk",
         age :23,
@@ -96,4 +97,4 @@ router.post('/votingage', function (req, res) {
            res.json(blankarray);
        })
     
- module.exports = router;
+ module.exports = router; */
