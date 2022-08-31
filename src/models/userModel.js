316e783f14b396d70e18d442c031f0a50e08270e
navdeep <pandatnavvu@gmail.com>
2022-09-01 03:38:30 +0530

    middw

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema( {
-    firstName: String,
+    FirstName: String,
     lastName: String,
+    address: String,
+    
     mobile: {
         type: String,
         unique: true,
